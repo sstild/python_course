665dca859a02342934467f8817ebad0ace1e77fe
Илья Соловьёв <solovyev.ie@gmail.com>
2026-06-16 09:09:36 +0300

    add 2 files from 1,2 lessons

---
+++ src/new.py
@@ -0,0 +1,17 @@
+a = 5
+b = 6
+c = a / b
+print(a + b,'     ',type(a),'     ',type(b))
+print(a - b,type(a),type(b))
+print(a * b,type(a),type(b))
+print(a / b,type(c),type(c))
+print(a // b,type(a),type(b))
+print(a % b,type(a),type(b))
+print(a ** b,type(a),type(b))
+
+d = '56\n25'
+#
+print(d)
+
+t="string'"
+print(type(t), t)
