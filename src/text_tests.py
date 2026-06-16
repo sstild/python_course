df783f8bca58378be0fc9a630b10378194b76ecd
Илья Соловьёв <solovyev.ie@gmail.com>
2026-06-16 09:59:26 +0300

    add text_test

---
+++ src/text_tests.py
@@ -0,0 +1,5 @@
+text = "Indeed CM обеспечивает полный контроль над ключевыми носителями на всех этапах их жизненного цикла."
+print(text)
+print(*text.split(), sep="\n")
+text = text.split()
+print(text)
